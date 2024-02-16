bd2147ff1546f84cd40f09bc94492c598ad9d026
CovetingAphid3 <tadiwachawanda@gmail.com>
2024-02-16 13:36:54 +0200

    edited auth page

--- frontend/src/AuthPage.jsx
+++ frontend/src/AuthPage.jsx
@@ -1,34 +1,51 @@
-import axios from "axios"
+import React, { useState } from "react";
+import axios from "axios";
 
 const AuthPage = (props) => {
-    const onSubmit = (e) => {
-      e.preventDefault();
-      const { value } = e.target[0];
-      axios.post(
-        'http://localhost:3001/authenticate',
-        {username:value}
-      )
-      .then(r=>props.onAuth({ ...r.data,secret:value }))
-      .catch(e=>console.log('error',e))
-    };
-  
-    return (
-      <div className="background">
-        <form onSubmit={onSubmit} className="form-card">
-          <div className="form-title">Welcome 👋</div>
-  
-          <div className="form-subtitle">Set a username to get started</div>
-  
-          <div className="auth">
-            <div className="auth-label">Username</div>
-            <input className="auth-input" name="username" />
-            <button className="auth-button" type="submit">
-              Enter
-            </button>
-          </div>
-        </form>
-      </div>
-    );
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post("http://localhost:3001/authenticate", { username, password })
+      .then((r) => props.onAuth({ ...r.data, secret: password }))
+      .catch((e) => console.log("error", e));
   };
-  
-  export default AuthPage;
+
+  return (
+    <div className="background">
+      <form onSubmit={onSubmit} className="form-card">
+        <div className="form-title">Welcome Back...</div>
+
+        <div className="form-subtitle">Login with a Username and Password:</div>
+
+        <div className="auth">
+          <div className="auth-label">Username</div>
+          <input
+            className="auth-input"
+            name="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+
+          <div className="auth-label-password">Password</div>
+          <input
+            className="auth-input"
+            name="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button className="auth-button" type="submit">
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AuthPage;
