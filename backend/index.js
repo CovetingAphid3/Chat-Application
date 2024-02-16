b24d9af2140eabac337bbe5330d9976b9fc0985e
CovetingAphid3 <tadiwachawanda@gmail.com>
2024-02-16 13:37:48 +0200

    authentication with password

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+// Server-side code
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
@@ -7,18 +8,24 @@ app.use(express.json());
 app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
-  const { username } = req.body;
+  const { username, password } = req.body;
 
   try {
     const r = await axios.put(
-        "https://api.chatengine.io/users/" ,
-        {username: username,secret: username,first_name: username},
-        {headers:{"private-key": "a7c09654-a412-4b21-878a-d53821af1276"}}
-    )
-    return res.status(r.status).json(r.data)
+      "https://api.chatengine.io/users/",
+      {
+        username: username,
+        secret: password, // Set the secret to the password value
+        first_name: username,
+      },
+      { headers: { "private-key": "a7c09654-a412-4b21-878a-d53821af1276" } }
+    );
+    return res.status(r.status).json(r.data);
   } catch (e) {
-    return res.status(e.response.status).json(e.response.data)
+    return res.status(e.response.status).json(e.response.data);
   }
 });
 
-app.listen(3001);
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
+});
